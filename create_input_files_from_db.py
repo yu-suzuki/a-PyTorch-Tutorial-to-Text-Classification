7c2fbb6216ed0bc596d38d44282ab54588ce42db
Yu Suzuki <suzuki_yu@icloud,com>
2019-08-30 16:36:16 +0900

    for sentences written in Japanese

--- create_input_files_from_db.py
+++ create_input_files_from_db.py
@@ -4,6 +4,8 @@ from utils import create_input_files_fromdb, train_word2vec_model
 if __name__ == '__main__':
     args = sys.argv
     create_input_files_fromdb(output_folder='./data'+args[1],
+                              hostname=args[2],
+                              database=args[3],
                        sentence_limit=15,
                        word_limit=20,
                        min_word_count=5)
